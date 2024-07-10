2857562f8cfe7202b787e6c9f44440392fa3c6fb
mohammed <mohammed01110834150@gmail.com>
2024-07-10 13:20:46 +0300

    layout

---
+++ resources/js/Layouts/GuestLayout.vue
@@ -0,0 +1,103 @@
+<template>
+    <v-layout class="rounded rounded-md">
+        <v-navigation-drawer
+            v-model="drawer"
+            :rail="rail"
+            permanent
+            @click="rail = false"
+            >
+
+            <v-list density="compact" nav>
+                <v-list-item prepend-icon="mdi-view-dashboard" title="Templates" value="Templates">
+                    <template v-slot:append>
+                        <v-btn
+                            icon="mdi-chevron-down"
+                            variant="text"
+                        ></v-btn>
+                    </template>
+                </v-list-item>
+                <v-list-item prepend-icon="mdi-format-text" title="Text" value="Text">
+                    <template v-slot:append>
+                        <v-btn
+                            icon="mdi-chevron-down"
+                            variant="text"
+                        ></v-btn>
+                    </template>
+                </v-list-item>
+                <v-list-item prepend-icon="mdi-image-outline" title="Photos" value="Photos">
+                    <template v-slot:append>
+                        <v-btn
+                            icon="mdi-chevron-down"
+                            variant="text"
+                        ></v-btn>
+                    </template>
+                </v-list-item>
+                <v-list-item prepend-icon="mdi-shape" title="Elements" value="Elements">
+                    <template v-slot:append>
+                        <v-btn
+                            icon="mdi-chevron-down"
+                            variant="text"
+                        ></v-btn>
+                    </template>
+                </v-list-item>
+                <v-list-item prepend-icon="mdi-cloud-upload" title="Upload" value="Upload">
+                    <template v-slot:append>
+                        <v-btn
+                            icon="mdi-chevron-down"
+                            variant="text"
+                        ></v-btn>
+                    </template>
+                </v-list-item>
+                <v-list-item prepend-icon="mdi-view-grid-compact" title="Background" value="Background">
+                    <template v-slot:append>
+                        <v-btn
+                            icon="mdi-chevron-down"
+                            variant="text"
+                        ></v-btn>
+                    </template>
+                </v-list-item>
+                <v-list-item prepend-icon="mdi-layers-triple" title="Layers" value="Layers">
+                    <template v-slot:append>
+                        <v-btn
+                            icon="mdi-chevron-down"
+                            variant="text"
+                        ></v-btn>
+                    </template>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-app-bar :elevation="1">
+            <template v-slot:prepend>
+                <v-app-bar-nav-icon variant="text" @click.stop="rail = !rail"></v-app-bar-nav-icon>
+                <v-btn icon="mdi-undo"></v-btn>
+                <v-btn icon="mdi-redo"></v-btn>
+            </template>
+
+            <template v-slot:append>
+                <v-btn>
+                    <v-icon icon="mdi-layers"></v-icon>
+                    position
+                </v-btn>
+                <v-btn>
+                    <v-icon icon="mdi-download"></v-icon>
+                    download
+                </v-btn>
+            </template>
+        </v-app-bar>
+
+        <v-main class="d-flex align-center justify-center" style="height: 100vh; background-color: #ebebeb;">
+            <slot />
+        </v-main>
+    </v-layout>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            drawer: true,
+            rail: true,
+        }
+    },
+}
+</script>
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,10 @@
+<template>
+    <Head title="Design" />
+    <guest-layout>
+        test
+    </guest-layout>
+</template>
+<script setup lang="ts">
+import GuestLayout from "../Layouts/GuestLayout.vue";
+import { Head } from '@inertiajs/vue3';
+</script>
